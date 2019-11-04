e69b41c3e619d56ff9aefe2e5b43a8969b6bf202
ssatra <satra.s@husky.neu.edu>
2019-11-04 18:34:59 -0500

    Added classes Datastore and AnalysisHelper

--- Assignment5_Xerox/src/com/assignment5/entities/Product.java
+++ Assignment5_Xerox/src/com/assignment5/entities/Product.java
@@ -5,10 +5,72 @@
  */
 package com.assignment5.entities;
 
+import java.util.List;
+
 /**
  *
  * @author kasai
  */
 public class Product {
+    private int productid;
+    private int minPrice;
+    private String Name;
+    private int Availability;
+    private String Description;
+    private List<Item> items;
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(int minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAvailability() {
+        return Availability;
+    }
+
+    public void setAvailability(int Availability) {
+        this.Availability = Availability;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "SalesPersonId is :"+productid+ "min proice "+ minPrice;
+    }
     
 }
--- Assignment5_Xerox/src/com/assignment5/entities/SalesPerson.java
+++ Assignment5_Xerox/src/com/assignment5/entities/SalesPerson.java
@@ -5,10 +5,37 @@
  */
 package com.assignment5.entities;
 
+import java.util.List;
+
 /**
  *
  * @author kasai
  */
 public class SalesPerson {
+    
+    private int salesPersonId;
+    private List<Item> solditems;
+
+    public int getSalesPersonId() {
+        return salesPersonId;
+    }
+
+    public void setSalesPersonId(int salesPersonId) {
+        this.salesPersonId = salesPersonId;
+    }
+
+    public List<Item> getSolditems() {
+        return solditems;
+    }
+
+    public void setSolditems(List<Item> solditems) {
+        this.solditems = solditems;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "SalesPersonId is :"+salesPersonId+" the total number of Items sold is : "+solditems.size();
+    }
     
 }
